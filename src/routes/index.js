991adfadf3160656ad9a5fafdd10c238b0095356
T-Huy <lehuy099@gmail.com>
2024-11-13 22:18:37 +0700

    Xóa toàn bộ mã nguồn và lịch sử

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Home from '~/pages/Home';
-
-//Public routes
-const publicRoutes = [{ path: '/', component: Home }];
-
-//Private routes
-const privateRoutes = [];
-
-export { publicRoutes, privateRoutes };
