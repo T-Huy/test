8c0470749a35e890d903f0d73584b30154e95de7
PTCanh <->
2024-11-16 18:27:05 +0700

    header + home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,67 +1,200 @@
 import { NavLink } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '~/context/UserContext';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { FiMenu } from 'react-icons/fi';
+import { FaRegAddressBook } from 'react-icons/fa';
+import { IoLogOutOutline } from 'react-icons/io5';
+import { IoMdKey } from 'react-icons/io';
 function Header() {
-    const navigate = useNavigate();
     const { user, logout } = useContext(UserContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     const handleLogout = () => {
         logout();
-        navigate('/');
-        toast.success('Đăng xuất thành công');
     };
 
     return (
-        <header className="w-full h-[57px] md:sticky top-0 bg-white z-[1000] border-b border-b-slate-100 flex items-center justify-between">
-            {/* Logo */}
-            <div className="flex items-center space-x-2 mr-auto p-4">
-                <span className="text-2xl font-bold">
-                    <span className="text-green-500">You</span>
-                    <span className="text-blue-600">Med</span>
-                </span>
-            </div>
+        <header className="bg-transparent shadow fixed top-0 left-0 right-0 z-50">
+            <div className="absolute inset-0 backdrop-blur-md bg-white/30"></div>
+            <div className="relative z-10 max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-20">
+                    {/* Logo */}
+                    <NavLink to="/" className="flex-shrink-0 flex items-center mr-auto">
+                        <span className="text-4xl font-bold">
+                            <span className="text-green-500">Easy</span>
+                            <span className="text-blue-500">Med</span>
+                        </span>
+                    </NavLink>
 
-            {/* Navigation */}
-            <nav className="bg-white absolute z-[100] top-0 right-0 left-0 transition transform origin-top-right h-screen lg:flex lg:h-auto overflow-scroll lg:overflow-visible lg:relative hidden">
-                <a href="#" className="hover:text-blue-600">
-                    Đặt khám
-                </a>
-                <a href="#" className="hover:text-blue-600">
-                    Tư vấn trực tuyến
-                </a>
-                <a href="#" className="hover:text-blue-600">
-                    Store
-                </a>
-                <a href="#" className="hover:text-blue-600">
-                    Tin Y tế
-                </a>
-                <a href="#" className="hover:text-blue-600">
-                    Dành cho nhân viên Y tế
-                </a>
-            </nav>
-
-            {/* Login Button */}
-            <div className="inline-flex space-x-3 items-center">
-                {user && !user.auth ? (
-                    <>
+                    {/* Navigation */}
+                    <nav className="hidden sm:ml-6 sm:flex sm:space-x-8 mr-32">
                         <NavLink
-                            to="/login"
-                            className="border border-blue-500 text-blue-600 px-4 py-1 rounded hover:bg-blue-100"
+                            to="#"
+                            className="inline-flex items-center px-1 pt-1 text-2xl font-bold text-gray-500 hover:text-gray-700"
                         >
-                            Đăng nhập
+                            Bệnh viện
                         </NavLink>
                         <NavLink
-                            to="/register"
-                            className="border border-blue-500 text-blue-600 px-4 py-1 rounded hover:bg-blue-100"
+                            to="#"
+                            className="inline-flex items-center px-1 pt-1 text-2xl font-bold text-gray-500 hover:text-gray-700"
                         >
-                            Đăng kí
+                            Bác sĩ
                         </NavLink>
-                    </>
-                ) : (
-                    <button onClick={handleLogout}>Logout</button>
-                )}
+                        <NavLink
+                            to="#"
+                            className="inline-flex items-center px-1 pt-1 text-2xl font-bold text-gray-500 hover:text-gray-700"
+                        >
+                            Chuyên khoa
+                        </NavLink>
+                    </nav>
+
+                    {/* Login Button */}
+                    <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                        {user && !user.auth ? (
+                            <>
+                                <NavLink
+                                    to="/login"
+                                    className="ml-4 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-xl text-white bg-blue-600 hover:bg-blue-700"
+                                >
+                                    Đăng nhập
+                                </NavLink>
+                                <NavLink
+                                    to="/register"
+                                    className="ml-4 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-xl text-white bg-blue-600 hover:bg-blue-700"
+                                >
+                                    Đăng kí
+                                </NavLink>
+                            </>
+                        ) : (
+                            <div
+                                className="relative inline-block text-left"
+                                onMouseEnter={() => setIsDropdownOpen(true)}
+                                onMouseLeave={() => setIsDropdownOpen(false)}
+                            >
+                                <div className="flex justify-center items-center cursor-pointer">
+                                    <div>
+                                        <img
+                                            className="w-14 h-14 rounded-full"
+                                            src="https://i.pinimg.com/736x/21/1a/6e/211a6e39c8db24b8cc967f3f0aeb8da0.jpg"
+                                            alt="Avatar"
+                                        />
+                                    </div>
+                                    <div className="ml-2">
+                                        <p className="text-2xl">Lê Tấn Huy</p>
+                                    </div>
+                                </div>
+                                {isDropdownOpen && (
+                                    <div className="absolute top-14 left-0 right-0 h-3 bg-transparent"></div>
+                                )}
+                                {isDropdownOpen && (
+                                    <div
+                                        className="absolute -right-6 backdrop-blur-md bg-white/30 mt-3 py-2 w-56 min-w-64 text-left rounded-xl border border-gray-200   shadow-[0_0_1px_0_rgba(0,0,0,0.04),0_2px_6px_0_rgba(0,0,0,0.04),0_10px_20px_0_rgba(0,0,0,0.04)]"
+                                        onMouseEnter={() => setIsDropdownOpen(true)}
+                                        onMouseLeave={() => setIsDropdownOpen(false)}
+                                    >
+                                        <ul className="py-1">
+                                            <li className="group px-4 py-2 text-left text-2xl font-medium hover:bg-slate-100 cursor-pointer flex items-center">
+                                                <NavLink
+                                                    to="/prifile"
+                                                    className={({ isActive }) =>
+                                                        isActive
+                                                            ? 'text-blue-500 flex items-center w-full'
+                                                            : 'flex items-center w-full'
+                                                    }
+                                                >
+                                                    <FaRegAddressBook className="mr-2 transform group-hover:animate-rotate-fast" />
+                                                    Hồ sơ cá nhân
+                                                </NavLink>
+                                            </li>
+                                            <li className="group px-4 py-2 text-left text-2xl font-medium hover:bg-slate-100 cursor-pointer flex items-center">
+                                                <NavLink
+                                                    to="/change-password"
+                                                    className={({ isActive }) =>
+                                                        isActive
+                                                            ? 'text-blue-500 flex items-center w-full'
+                                                            : 'flex items-center w-full'
+                                                    }
+                                                >
+                                                    <IoMdKey className="mr-2 transform group-hover:animate-rotate-fast" />
+                                                    Đổi mật khẩu
+                                                </NavLink>
+                                            </li>
+                                            <li
+                                                className="group px-4 py-2 text-left text-2xl text-[#e74c3c] font-medium hover:bg-slate-100 cursor-pointer flex items-center"
+                                                onClick={handleLogout}
+                                            >
+                                                <IoLogOutOutline className="mr-2 transform group-hover:animate-rotate-fast" />
+                                                Đăng xuất
+                                            </li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                            // <NavLink
+                            //     className="ml-4 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700"
+                            //     onClick={handleLogout}
+                            // >
+                            //     Logout
+                            // </NavLink>
+                        )}
+                    </div>
+                    <div className="p-5 flex items-center sm:hidden" onClick={() => setIsOpen(!isOpen)}>
+                        <FiMenu />
+                    </div>
+                </div>
             </div>
+
+            {/* Mobile Menu */}
+            {isOpen && (
+                <div className="sm:hidden">
+                    <div className="pt-2 pb-3 space-y-1">
+                        <NavLink
+                            to="#"
+                            className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-50 hover:border-gray-300"
+                        >
+                            Bệnh viện
+                        </NavLink>
+                        <NavLink
+                            to="#"
+                            className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-50 hover:border-gray-300"
+                        >
+                            Bác sĩ
+                        </NavLink>
+                        <NavLink
+                            to="#"
+                            className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-50 hover:border-gray-300"
+                        >
+                            Chuyên khoa
+                        </NavLink>
+                    </div>
+                    <div className="pb-3 border-t border-gray-200">
+                        {user && !user.auth ? (
+                            <>
+                                <NavLink
+                                    to="/login"
+                                    className="block px-4 py-2 text-base font-medium text-center text-white bg-blue-600 hover:bg-blue-700"
+                                >
+                                    Đăng nhập
+                                </NavLink>
+                                <NavLink
+                                    to="/register"
+                                    className="block px-4 py-2 mt-2 text-base font-medium text-center text-white bg-blue-600 hover:bg-blue-700"
+                                >
+                                    Đăng kí
+                                </NavLink>
+                            </>
+                        ) : (
+                            <NavLink
+                                className="block px-4 py-2 text-base font-medium text-center text-white bg-blue-600 hover:bg-blue-700"
+                                onClick={handleLogout}
+                            >
+                                Logout
+                            </NavLink>
+                        )}
+                    </div>
+                </div>
+            )}
         </header>
     );
 }
